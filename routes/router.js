06fb73867c2daed303e055547fe00da2898621b2
Rafiansyah Dwi Sadyawinata <rafiansyah1931033@itpln.ac.id>
2023-07-28 11:42:46 +0700

    REFINE CODE

--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,7 @@ const Comment = require('../models/comment.js');
 
 router.get('/video-thumbnails', async (req, res) => {
   try {
-    const thumbnails = await VideoThumbnail.find();
+    const thumbnails = await VideoThumbnail.find({}, { _id: 0, __v: 0 });
     res.json(thumbnails);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
@@ -16,7 +16,7 @@ router.get('/video-thumbnails', async (req, res) => {
 router.get('/products/:videoID', async (req, res) => {
   const videoID = req.params.videoID;
   try {
-    const products = await Product.find({ videoID });
+    const products = await Product.find({ videoID },{ _id: 0, __v: 0, videoID: 0});
     res.json(products);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
@@ -26,7 +26,7 @@ router.get('/products/:videoID', async (req, res) => {
 router.get('/comments/:videoID', async (req, res) => {
   const videoID = req.params.videoID;
   try {
-    const comments = await Comment.find({ videoID });
+    const comments = await Comment.find({ videoID },{ _id: 0, __v: 0, videoID: 0 });
     res.json(comments);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
